dd9009ea3c111f5d1ea39d7646fd0eb02dd389ae
KeerthikaKudali <keerthikakudali@gmail.com>
2025-07-13 13:17:37 +0530

    Create ExternalApi.java

---
+++ Week 2/TDD Using JUnit5 and Mockito/Mocking and Stubbing/ExternalApi.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// This interface represents an external API that provides data.
+public interface ExternalApi {
+    String getData(); // Method to fetch data from the external API
+}
+
