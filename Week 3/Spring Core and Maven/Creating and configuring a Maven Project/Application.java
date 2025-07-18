b5488a649b51bf99566e86a1c9d3343ab1a3f46e
KeerthikaKudali <keerthikakudali@gmail.com>
2025-07-18 19:11:33 +0530

    Create Application.java

---
+++ Week 3/Spring Core and Maven/Creating and configuring a Maven Project/Application.java
@@ -0,0 +1,7 @@
+package org.example;
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Library Management Application Starting...");
+        System.out.println("Setup complete. You can now start building your application logic.");
+    }
+}
