0218dbfac05ae3d48b5688ab6f6664b40c2b0967
KeerthikaKudali <keerthikakudali@gmail.com>
2025-07-20 15:08:11 +0530

    feat: Add Hello World RESTful Web Service on /hello

---
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.cognizant.spring_learn.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@RestController
+public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    @GetMapping("/hello")
+    public String sayHello() {
+        LOGGER.info("START");
+        LOGGER.debug("Hello World!!");
+        LOGGER.info("END");
+        return "Hello World!!";
+    }
+}
