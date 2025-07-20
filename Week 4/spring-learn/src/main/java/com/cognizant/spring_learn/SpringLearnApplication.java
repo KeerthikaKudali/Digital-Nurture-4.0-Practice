6448eb5b68ede24c4928d23a7ace1526bf135ead
KeerthikaKudali <keerthikakudali@gmail.com>
2025-07-20 13:35:03 +0530

    Implement Spring Core XML config for SimpleDateFormat (Hands-on 2)

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,18 +2,39 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger; // Import Logger
-import org.slf4j.LoggerFactory; // Import LoggerFactory
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 @SpringBootApplication
 public class SpringLearnApplication {
-
-    // Declare a logger for this class
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
-        // Add a log message after the application has started
         LOGGER.info("SpringLearnApplication started successfully!");
+
+        displayDate();
+    }
+
+    private static void displayDate() {
+        LOGGER.info("START");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        String dateString = "31/12/2018";
+        try {
+            Date parsedDate = format.parse(dateString);
+            System.out.println("Parsed Date: " + parsedDate);
+            LOGGER.debug("Parsed Date: {}", parsedDate);
+        } catch (ParseException e) {
+            LOGGER.error("Error parsing date: {}", dateString, e);
+            System.err.println("Error parsing date: " + dateString + " - " + e.getMessage());
+        }
+        LOGGER.info("END");
     }
 }
