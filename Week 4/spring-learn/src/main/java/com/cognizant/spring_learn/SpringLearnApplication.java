f73727451014d331ff88daaae2ad951f56399143
KeerthikaKudali <keerthikakudali@gmail.com>
2025-07-20 13:15:15 +0530

    Update SpringLearnApplication.java

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,18 +2,13 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger; // Import Logger
-import org.slf4j.LoggerFactory; // Import LoggerFactory
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class SpringLearnApplication {
-
-    // Declare a logger for this class
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
-        // Add a log message after the application has started
         LOGGER.info("SpringLearnApplication started successfully!");
     }
 }
